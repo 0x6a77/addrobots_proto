eb845182f923908a6b92137d378b15ae50f5ac84
jeff.williams <jeff.williams@addrobots.com>
2018-06-25 20:46:29 -0700

    #7 preliminary work to publish api docs as a github release

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 echo 'java'
-protoc --proto_path=./SourceMessages/ --java_out=./Production/Java/ ./SourceMessages/VehicleMsg.proto
-protoc --proto_path=./SourceMessages/ --java_out=./Production/Java/ ./SourceMessages/MotorMsg.proto
+protoc --proto_path=./src/protobuf/ --java_out=./generated/Java/ ./src/protobuf/VehicleMsg.proto
+protoc --proto_path=./src/protobuf/ --java_out=./generated/Java/ ./src/protobuf/MotorMsg.proto
 
 echo 'javascript'
-protoc --proto_path=./SourceMessages/ --js_out=import_style=commonjs,binary:./Production/Javascript/ ./SourceMessages/VehicleMsg.proto
-protoc --proto_path=./SourceMessages/ --js_out=import_style=commonjs,binary:./Production/Javascript/ ./SourceMessages/MotorMsg.proto
+protoc --proto_path=./src/protobuf/ --js_out=import_style=commonjs,binary:./generated/Javascript/ ./src/protobuf/VehicleMsg.proto
+protoc --proto_path=./src/protobuf/ --js_out=import_style=commonjs,binary:./generated/Javascript/ ./src/protobuf/MotorMsg.proto
 
 echo 'python'
-protoc --proto_path=./SourceMessages/ --python_out=./Production/Python/ ./SourceMessages/VehicleMsg.proto
-protoc --proto_path=./SourceMessages/ --python_out=./Production/Python/ ./SourceMessages/MotorMsg.proto
+protoc --proto_path=./src/protobuf/ --python_out=./generated/Python/ ./src/protobuf/VehicleMsg.proto
+protoc --proto_path=./src/protobuf/ --python_out=./generated/Python/ ./src/protobuf/MotorMsg.proto
 
 echo 'embedded-c'
-cd Production
-protoc --plugin=nanopb=~/git/nanopb/dist/nanopb-0.3.8-28-g14efb1a-macosx-x86/generator/protoc-gen-nanopb --include_imports=~/git/nanopb/dist/nanopb-0.3.8-28-g14efb1a-macosx-x86/generator/proto/nanopb.proto --proto_path=../SourceMessages -o./MotorMsg.pb ../SourceMessages/MotorMsg.proto
-python ~/git/nanopb/dist/nanopb-0.3.8-28-g14efb1a-macosx-x86/generator/nanopb_generator.py -f ../SourceMessages/MotorMsg.options --output-dir=../Production/NanoPB MotorMsg.pb
+cd generated
+protoc --plugin=nanopb=~/git/nanopb/dist/nanopb-0.3.8-28-g14efb1a-macosx-x86/generator/protoc-gen-nanopb --include_imports=~/git/nanopb/dist/nanopb-0.3.8-28-g14efb1a-macosx-x86/generator/proto/nanopb.proto --proto_path=../src/protobuf -o./MotorMsg.pb ../src/protobuf/MotorMsg.proto
+python ~/git/nanopb/dist/nanopb-0.3.8-28-g14efb1a-macosx-x86/generator/nanopb_generator.py -f ../src/protobuf/MotorMsg.options --output-dir=../generated/NanoPB MotorMsg.pb
